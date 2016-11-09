3cf5be42612afd30f3a962a034545d90bf66c553
Yocy <codehuman@163.com>
2016-11-09 11:17:30 +0800

    letcode-1 twoSum

--- letcode/letcode-1.java
+++ letcode/letcode-1.java
@@ -13,4 +13,20 @@ public class Solution{
 		}
 		return res;
 	}
+	public int[] twoSum2(int[] nums, target){
+		int n = nums.length;
+		int res[] = new int[2];
+		HashMap<Integer, Integer> map=new HashMap<Integer, Integer>();
+		for(int i = 0; i< n; i++){
+			if(map.containsKey(nums[i])){
+				int index = map.get(nums[i]);
+				res[0] = index;
+				res[1] = i;
+				return res;
+			}else{
+				map.put(target-nums[i], i);
+			}
+		}
+		return res;
+	}
 }
