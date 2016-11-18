d3a9a383817b81a43bae38e3c8dd264b57e8f5e1
Yocy <codehuman@163.com>
2016-11-18 21:41:08 +0800

    Valid Parentheses]

--- letcode/Letcode20.java
+++ letcode/Letcode20.java
@@ -1,33 +1,23 @@
 public class letcode20{
 	public boolean validParentheses(String s){
-		Stack stackA = new Stack();
+		Stack<Character> stackA = new Stack<Character>();
 		int n = s.length();
 		for(int i = 0; i< n; i++){
 			char c = s.charAt(i);
 			if(c=='{'|| c == '(' || c== '['){
 				stackA.push(c);
-			}else if(c == ')'){
-				if(!stackA.empty()){
-					if(!stackA.pop().equals('(')){
-						return false;
-					}
+			}else {
+				if(stackA.empty()){
+					return false;
 				}
-				return false;
-			}else if(c==']'){
-				if(!stackA.empty()){
-					if(!stackA.pop().equals('[')){
-						return false;
-
-					}
+				char top = stackA.pop();
+				if(s.charAt(i)==')'&&top!='('){
+					return false;
+				}else if(s.charAt(i)==']'&&top!='['){
+					return false;
+				}else if(s.charAt(i)=='}'&& top!='{'){
+					return false;
 				}
-				return false;
-			}else if(c == '}'){
-				if(!stackA.empty()){
-					if(!stackA.pop().equals('{')){
-						return false;
-					}
-				}
-				return false;
 			}
 		}
 		return stackA.empty();
