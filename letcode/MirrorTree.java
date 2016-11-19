b932eb2d2b05eb1e638ea20de01615d44a7623f2
Yocy <codehuman@163.com>
2016-11-19 19:22:31 +0800

    镜像二叉树

---
+++ letcode/MirrorTree.java
@@ -0,0 +1,19 @@
+public class MirrorTree{
+	public void reverse(TreeNode root){
+		if(root == null){
+			return;
+		}
+		if(root.left==null && root.right == null){
+			return;
+		}
+		TreeNode tmp = root.left;
+		root.left = root.right;
+		root.right = tmp;
+		if(root.left != null){
+			reverse(root.left);
+		}
+		if(root.right != null){
+			reverse(root.right);
+		}
+	}
+}
