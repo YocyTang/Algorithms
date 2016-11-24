81d9c417cb336498f9cdfac5a6435ee72e1a3680
Yocy <codehuman@163.com>
2016-11-24 17:01:21 +0800

    Find TreeNode Path

---
+++ letcode/FindTreePath.java
@@ -0,0 +1,28 @@
+public class FindTreePath{
+	//二叉树寻找和为固定数的路径（到底部）
+	public <List<List<Integer>> find(TreeNode root, int sum){
+		List<List<Integer>> res = new ArrayList<List<Integer>>();
+		if(root == null){
+			return res;
+		}
+		List<Integer> tmp = new  ArrayList<Integer>();
+		int curr = 0;
+		find(root, sum, res, tmp, curr);
+		return res;
+	}
+	private void find(TreeNode root, int sum, List<List<Integer>> res, List<Integer> tmp, int curr){
+		curr+= root.val;
+		tmp.add(root.val);
+		if(root.left == null && root.right == null && curr ==sum){
+			res.add(new ArrayList<Integer>(tmp));
+			return;
+		}
+		if(root.left!=null){
+			find(root.left, sum, res, tmp, curr);
+		}
+		if(root.right != null){
+			find(root.right, sum , res, tmp, curr);
+		}
+		tmp.remove(tmp.size()-1);
+	}
+}
