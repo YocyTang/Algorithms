8603a6c46db6706a18671b1539a59d1abd52c435
Yocy <codehuman@163.com>
2016-11-26 21:25:18 +0800

    ugly number 丑数 2016-11-26

---
+++ letcode/UglyNumber.java
@@ -0,0 +1,26 @@
+public class uglyNumber{
+	public int nthUglyNumber(int index){
+		if(index<=0){
+			return 0;
+		}
+		int[] nums = new int[index];
+		nums[0] = 1;
+		int index2 = index3 = index5 = 0;
+		int nextIndex = 1;
+		while(nextIndex < index){
+			int min = Math.min(nums[index2]*2, nums[index3]*3, nums[index5]*5);
+			nums[nextIndex];
+			while(nums[index2]*2<min){
+				index2++;
+			}
+			while(nums[index3]*3<min){
+				index3++;
+			}
+			while(nums[index5]*5<min){
+				index5++;
+			}
+			nextIndex++;
+		}
+		return nums[index-1];
+	}
+}
