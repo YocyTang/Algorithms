39be24a6fa9d93bb15d391bd917f45491e297d82
Yocy <codehuman@163.com>
2016-12-06 20:06:24 +0800

    CountBit-java,计算int中二进制1的个数（两种解法）

--- letcode/CountBit.java
+++ letcode/CountBit.java
@@ -2,20 +2,21 @@ public class CountBit{
 	//计算整数二进制1的个数
 	public int solution1(int n){
 		int count = 0;
-		int helper = 1;
-		while(helper){
-			if(n&helper){
+		int flag= 1;
+		while(flag!=Math.pow(2, 7)){
+			if((flag & n) ==1){
 				count++;
 			}
-			helper=helper<<1;
+			flag = flag<<1;
 		}
 		return count;
 	}
 	public int solution2(int n){
 		int count =0;
-		while(n){
+		while(n>0){
 			count++;
 			n = (n-1)&n;
 		}
+		return count;
 	}
 }
