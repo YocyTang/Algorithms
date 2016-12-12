1a0ceae694a7fbc35a94704ba9039db50c933be9
Yocy <codehuman@163.com>
2016-12-12 23:15:03 +0800

    letcode 59--顺时针建立矩阵

--- letcode/SpiralMatrixII.java
+++ letcode/SpiralMatrixII.java
@@ -17,19 +17,16 @@ private void buildMatrix(int start, int endIndex, int index, int[][] res, int le
 	int end  =len-start-1;
 	for(int i = start; i<=end; i++){
 		res[start][i] = index++;
+	}	
+	for(int i = start+1; i<=end;i++){
+		res[i][end] = index++;
 	}
-	if(start<end){
-		for(int i = start+1; i<=end;i++){
-			res[i][end] = index++;
-		}
-		for(int i = end-1; i>=start; i--){
-			res[end][i] = index++;
-		}
-		if(start<end-1){
-			for(int i = end-1; i>=start+1; i--){
-				res[i][start] = index++;
-			}
-		}
+	for(int i = end-1; i>=start; i--){
+		res[end][i] = index++;
+	}
+		
+	for(int i = end-1; i>=start+1; i--){
+		res[i][start] = index++;
 	}
 	start= start+1;
 	buildMatrix(start, endIndex, index, res, len);
