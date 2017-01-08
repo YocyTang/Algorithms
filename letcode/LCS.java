de6020b5bf5afd65f0e7946f8fb492abcf2586f8
Yocy <codehuman@163.com>
2017-01-08 21:19:17 +0800

    最长公共子序列 动态规划

--- letcode/LCS.java
+++ letcode/LCS.java
@@ -2,16 +2,14 @@ public static int lcs(char[] x, char[] y){
 	int m = x.length;
 	int n = y.length;
 	int[][] dp  = new int[m+1][n+1];
-	int max = 0;
 	for(int i = 1; i <= m; i++){
 		for(int j = 1; j<= n; j++){
 			if(x[i-1] == y[j-1]){
 				dp[i][j] = dp[i-1][j-1]+1;
 			}else{
 				dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-			}
-			max = Math.max(max, dp[i][j]);
+			}			
 		}
 	}
-	return max;
+	return dp[m][n];
 }
