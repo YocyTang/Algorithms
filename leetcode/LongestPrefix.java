f3853a106bf1b912a7fc1198d34697857b1bf810
Yocy <codehuman@163.com>
2017-01-16 21:25:55 +0800

    最长前缀字符串

---
+++ leetcode/LongestPrefix.java
@@ -0,0 +1,22 @@
+public class LongestPrefix{
+	public String longestPrefix(String[] strs){
+		if(strs==null || strs.length==0){
+			return "";
+		}
+		String key = strs[0];
+		int end = key.length();
+		int len = strs.length;
+		for(int i=1; i<len; i++){
+			int j = 0;
+			int k= strs[i].length();
+			for(;j<k&&j<end;j++){
+				if(strs[i].charAt(j)!=key.charAt(j)){
+					break;
+				}
+			}
+			end=j;
+		}
+		return key.substring(0, end);
+
+	}
+}
