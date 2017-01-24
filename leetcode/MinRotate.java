2555bb7cd72928b7ba64495d0ad4d9d948d4d3b0
Yocy <codehuman@163.com>
2017-01-24 11:23:23 +0800

    旋转数组的最小数

--- leetcode/MinRotate.java
+++ leetcode/MinRotate.java
@@ -1,34 +1,34 @@
-public int MinRotate(int[] nums){
-	if(nums == null||nums.length ==0){
-		throw Exception;
+public static int MinRotate(int[] nums){
+	if(nums == null || nums.length == 0){
+		throw new Exception();
 	}
 	int n = nums.length;
-	int index1 =0;
-	int index2 = n-1;
-	int mid = 0;
-	while(nums[index1]>=nums[index2]){
-		if(index2-index1 ==1) {
-			mid = index2;
+	int lo = 0;
+	int hi = n-1;
+	int mid =  lo;
+	while(nums[lo]>=nums[hi]){
+		if(hi-lo == 1){
+			mid = lo;
 			break;
+		} 
+		mid = lo+(hi-lo)/2;
+		if(nums[lo]==nums[hi]&&nums[hi] = nums[mid]){
+			return findMin(nums, lo, hi);
 		}
-		mid = (index1+index2)/2;
-
-		if(nums[index1] == nums[index2]&& nums[mmid] == nums[index1]){
-			return MinOrder(nums, index1, index2);
+		if(nums[lo]<=nums[mid]){
+			lo =mid;
+		}else if(nums[mid]<=nums[hi]){
+			hi = mid;
 		}
-		if(nums[mid]>=nums[index1]){
-			index1 = mid;
-		}else{
-			index2 = mid;
-		}
-		return nums[mid];
 	}
-	private int MinOrder(int nums, int start, int end){
-		int res = nums[index1];
-		for(int i = index1+1; i<=index2; i++){
-			if(res>nums[i])
-				res = nums[i];
+	return nums[mid];
+}
+private int findMin(int[] nums, int lo, int hi){
+	int res = nums[lo];
+	for(int i = lo+1; i<=hi; i++){
+		if(res <nums[i]){
+			res = nums[i];
 		}
-		return res;
 	}
+	return res;
 }
