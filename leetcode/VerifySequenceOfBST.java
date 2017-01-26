97f40b1e7711944ad96ab6dfd5dbe505d1d14371
Yocy <codehuman@163.com>
2017-01-26 08:59:23 +0800

    判断后续遍历是否为某个二叉搜索树

--- leetcode/VerifySequenceOfBST.java
+++ leetcode/VerifySequenceOfBST.java
@@ -1,33 +1,35 @@
 public class VerifySequenceOfBST{
 	//后序遍历是否为二叉搜索树
 	public boolean verify(int[] order){
-		if(order == null || order.length == 0){
+		if(order== null || order.length == 0){
 			return false;
 		}
-		int end = order.length-1;
-		return verify(order, 0, end);
+		int end= order.length;
+		return core(order, 0, end);
 	}
-	private boolean verify(int[] order,int start, int end){
-		int root = order[end],
-			index = start;
-		while(index<end+1){
-			if(order[index] > root)
+	private boolean core(int[] order, int start, int end){
+		int root = order[end]; 
+		int index = start;
+		while(index<end){
+			if(order[index]>root){
 				break;
+			}
 			index++;
 		}
-		for(int i = index; i< end+1; i++){
-			if(order[index] <root){
+		for(int i = index; i< end; i++){
+			if(order[i] < root){
 				return false;
 			}
 		}
-		boolean left  = true;
-		if(index > 0){
-			left = verify(oder, start,index-1);
+		boolean left = true;
+		if(index >0){
+			left = core(order, start,index-1);
+
 		}
 		boolean right = true;
-		if(index <end){
-			right = verify(order, index, end);
+		if(index<end){
+			right = core(order, index, end);
 		}
-		return left&&right;
+		return left&&end;
 	}
 }
