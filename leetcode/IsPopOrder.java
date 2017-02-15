5d070de1de0afbc076403e7a3fa40ec73f9e6b3c
Yocy <codehuman@163.com>
2017-02-15 09:30:04 +0800

    判断栈的弹出序列

--- leetcode/IsPopOrder.java
+++ leetcode/IsPopOrder.java
@@ -5,7 +5,7 @@ public boolean  isPopOrder(int[] push, int[] pop){
 	int n = push.length;
 	LinkedList<Integer> stack = new LinkedList<Integer>();
 	int popIndex = 0;
-	for(int pushIndex = 0; pushIndex< n; i++){
+	for(int pushIndex = 0; pushIndex< n; pushIndex++){
 		stack.addFirst(push[pushIndex]);
 		while(!stack.isEmpty() && stack.getFirst() == pop[popIndex]){
 			stack.pop();
