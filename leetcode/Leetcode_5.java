e94921ca30c52c47ca2bd07571007f6c7667518e
Yocy <codehuman@163.com>
2017-02-25 09:25:44 +0800

    最长回文字符串--动态规划解法

--- leetcode/Leetcode_5.java
+++ leetcode/Leetcode_5.java
@@ -1,36 +1,33 @@
 public class Letcode_5{
 	public String longestPali(String s){
 		if(s == null || s.length() == 0){
-			return "";
+			return null;
 		}
-		int max = 0;
-		int start = 0;
-		int end = 0;
 		int n = s.length();
 		boolean[][] dp = new boolean[n][n];
-		for(int i = 0; i<n;i++){
+		for(int i = 0;i<n;i++){
 			for(int j = 0; j<n; j++){
 				if(i>=j){
-					dp[i][j]=true;
-				}else{
-					dp[i][j] = false;
+					dp[i][j] = true;
 				}
 			}
 		}
-		for(int j = 1; j<n;j++){
-			for(int i = 0; i<j;i++){
+		int max =0;
+		int start = 0;
+		int end = 0;
+		for (int j = 1; j<n; j++ ) {
+			for(int i = 0; i<j; i++){
 				if(s.charAt(i) == s.charAt(j)&& dp[i+1][j-1]){
 					dp[i][j] = true;
 					if(max<j-i+1){
 						max = j-i+1;
 						start = i;
-						end = j;
+						end = j+1;
 					}
-				}else{
-					dp[i][j] = false;
 				}
+
 			}
 		}
-		return s.substring(start, end+1);
+		return s.substring(start, end);
 	}
 }
