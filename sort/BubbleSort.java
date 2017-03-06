fcb26c515808c045604bb1731c7df2ee7b11c2b3
Yocy <codehuman@163.com>
2017-03-06 11:22:25 +0800

    bubble

---
+++ sort/BubbleSort.java
@@ -0,0 +1,17 @@
+public static void sort(int[] a){
+	if(a == null){
+		return ;
+	}
+	for(int i = 0; i< a.length; i++){
+		for(int j = 0;j< a.length-1-j; j++){
+			if(a[j]>a[j+1]){
+				swap(a, j+1, j);
+			}
+		}
+	}
+}
+static void swap(int[] a, int lo, int hi){
+	int temp = a[lo];
+	a[lo] = a[hi];
+	a[hi] = temp;
+}
